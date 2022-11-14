a6295ccd31a80064dcce951213c4ec7026aba82e
dzako <jcyranka@gmail.com>
2022-11-14 17:23:53 +0100

    minor refactor complete results N 10,20

--- matrixA_estimator_componentwise.py
+++ matrixA_estimator_componentwise.py
@@ -8,70 +8,75 @@ import torch
 from torch import nn, optim
 import torch.nn.functional as F
 
-#a simple NN controller
+# a simple NN controller
 class ComponentwiseANet(nn.Module):
     name = "Net"
-    def __init__(self):
+
+    def __init__(self, N):
         super().__init__()
-        self.dim = 10
-        self.fc1 = nn.Linear(self.dim, self.dim, bias = False)
-        self.fc2 = nn.Linear(self.dim, self.dim, bias = False)
+        self.dim = N
+        self.fc1 = nn.Linear(self.dim, self.dim, bias=False)
+        self.fc2 = nn.Linear(self.dim, self.dim, bias=False)
 
     def forward(self, x0, x1):
         r0 = torch.sum(self.fc1(x0) * x0, dim=-1)
         r1 = torch.sum(self.fc2(x1) * x1, dim=-1)
         return r0 + r1
 
+
 def L(x):
-    return np.sum([ np.sum((c - x) ** 2) for c in x ])
+    return np.sum([np.sum((c - x) ** 2) for c in x])
 
 
+suffix = "28paz-20-10k-005"
 
-filesn0 = ['X0-24paz-10-100k.csv']
-filesn1 = ['X1-24paz-10-100k.csv']
-dirn = 'data/10/'
+N = 20
+filesn0 = [f"X0-{suffix}.csv"]
+filesn1 = [f"X1-{suffix}.csv"]
+
 x0s = []
 x1s = []
-N = 10
+
+dirn = f"data/{N}/"
+outdir = f"results/componentwise/{N}/"
 T = 1
 fixedI = 0
-component = 2
 
 for fn in filesn0:
-    #loads data
-    X0 = np.loadtxt(dirn + fn, delimiter = ',')
-    x0s.append( np.reshape(X0, (X0.shape[0], N, 1)) )
-for fn in filesn1:    
-    X1 = np.loadtxt(dirn + fn, delimiter = ',')
-    x1s.append( np.reshape(X1, (X1.shape[0], N, 1)) )
+    # loads data
+    X0 = np.loadtxt(dirn + fn, delimiter=",")
+    x0s.append(np.reshape(X0, (X0.shape[0], N, 1)))
+for fn in filesn1:
+    X1 = np.loadtxt(dirn + fn, delimiter=",")
+    x1s.append(np.reshape(X1, (X1.shape[0], N, 1)))
 
-newX0 = np.concatenate(x0s, axis = 0)
-newX1 = np.concatenate(x1s, axis = 0)
+newX0 = np.concatenate(x0s, axis=0)
+newX1 = np.concatenate(x1s, axis=0)
 print(newX0.shape, newX1.shape)
 
-#labels for the first particle
+# labels for the first particle
 ys = []
-fileCn = [ 'CCC-24paz-10-100k.csv' ]
+fileCn = [f"CCC-{suffix}.csv"]
 for fn in fileCn:
-    Y = np.loadtxt(dirn + fn, delimiter = ',').astype(int)
-    ys.append( Y )
+    Y = np.loadtxt(dirn + fn, delimiter=",").astype(int)
+    ys.append(Y)
 
-Y_logits = np.concatenate(ys, axis = 0)
+Y_logits = np.concatenate(ys, axis=0)
 
-#load labels
-fileLn = ['LLL-24paz-10-100k.cvs']
+# load labels
+fileLn = [f"LLL-{suffix}.cvs"]
 ls = []
 for fn in fileLn:
-    L = np.loadtxt(dirn + fn, delimiter = ',')
-    ls.append(L)
+    Ll = np.loadtxt(dirn + fn, delimiter=",")
+    ls.append(Ll)
 
-Labels = np.concatenate(ls, axis = 0)
+Labels = np.concatenate(ls, axis=0)
 
 print(newX0[0])
 print(newX1[0])
 print(Labels[0])
 
-#group rows by c particle
+# group rows by c particle
 inds = []
 X0_group = []
 X1_group = []
@@ -81,52 +86,59 @@ tildeX_group = []
 for i in range(N):
     curind = np.where(Y_logits == i)[0]
     print(f"group particle {i} len {len(curind)}")
-    inds.append( curind )
-    X0_group.append( newX0[ curind ].reshape((len(curind), N)) )
-    X1_group.append( newX1[ curind ].reshape((len(curind), N)) )
-    Labels_group.append( Labels[ curind ] )
+    inds.append(curind)
+    X0_group.append(newX0[curind].reshape((len(curind), N)))
+    X1_group.append(newX1[curind].reshape((len(curind), N)))
+    Labels_group.append(Labels[curind])
 
 print(X0_group[0].shape, X1_group[0].shape, Labels_group[0].shape)
 
-currentg = 2
+for currentg in range(N):
+    print(f"currentg={currentg}")
+    print("groups")
+    print(X0_group[currentg][0])
+    print(X1_group[currentg][0])
+    print(Labels_group[currentg][0])
 
-print("groups")
-print(X0_group[currentg][0])
-print(X1_group[currentg][0])
-print(Labels_group[currentg][0])
+    D = X0_group[currentg].shape[0]
 
-N = X0_group[currentg].shape[0]
+    Dtrain = int(0.9 * D)
 
-Ntrain = int(0.9 * N)
+    X0_train = torch.FloatTensor(X0_group[currentg])[:Dtrain]
+    X1_train = torch.FloatTensor(X1_group[currentg])[:Dtrain]
+    Y_train = Labels_group[currentg][:Dtrain]
 
-X0_train = torch.FloatTensor(X0_group[currentg])[:Ntrain]
-X1_train = torch.FloatTensor(X1_group[currentg])[:Ntrain]
-Y_train = Labels_group[currentg][:Ntrain]
+    X0_test = torch.FloatTensor(X0_group[currentg])[Dtrain:]
+    X1_test = torch.FloatTensor(X1_group[currentg])[Dtrain:]
+    Y_test = Labels_group[currentg][Dtrain:]
 
-X0_test = torch.FloatTensor(X0_group[currentg])[Ntrain:]
-X1_test = torch.FloatTensor(X1_group[currentg])[Ntrain:]
-Y_test = Labels_group[currentg][Ntrain:]
+    net = ComponentwiseANet(N)
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    loss = nn.MSELoss()
 
+    for i in range(15000):
+        optimizer.zero_grad()
+        out = net(X0_train, X1_train)
+        l = loss(out, torch.squeeze(torch.FloatTensor(Y_train)))
+        l.backward()
+        optimizer.step()
+        if i % 1000 == 0:
+            print(l.item())
 
-net = ComponentwiseANet()
-optimizer = optim.Adam(net.parameters(), lr=0.001)
-loss = nn.MSELoss()
+    net.eval()
 
-for i in range(20000):
-    optimizer.zero_grad()
-    out = net( X0_train, X1_train )
-    l = loss(out, torch.squeeze(torch.FloatTensor(Y_train)))
-    l.backward()
-    optimizer.step()
-    print(l.item())
+    r = net(X0_test, X1_test)
 
-net.eval()
+    print(f"{currentg} EVALUATION")
+    print(np.sum((r.detach().numpy() - Y_test) ** 2) / (D - Dtrain))
 
-r = net(X0_test, X1_test)
-
-print(r[:10], Y_test[:10])
-
-print( np.sum( (r.detach().numpy() - Y_test) ** 2 ) / (N-Ntrain) )
-
-np.savetxt(f'A_0_{fixedI}{currentg}.csv', net.fc1.weight.detach().numpy() + net.fc1.weight.detach().numpy().T, delimiter=',' )
-np.savetxt(f'A_1_{fixedI}{currentg}.csv', net.fc2.weight.detach().numpy() + net.fc2.weight.detach().numpy().T, delimiter=',' )
+    np.savetxt(
+        f"{outdir}A_0_{fixedI}{currentg}.csv",
+        net.fc1.weight.detach().numpy() + net.fc1.weight.detach().numpy().T,
+        delimiter=",",
+    )
+    np.savetxt(
+        f"{outdir}A_1_{fixedI}{currentg}.csv",
+        net.fc2.weight.detach().numpy() + net.fc2.weight.detach().numpy().T,
+        delimiter=",",
+    )
